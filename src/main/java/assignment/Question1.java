276994e4f475ed35cef75ce588e06a05a44ec7b8
vijay2340025@gmail.com <vijay9952457132>
2022-01-25 16:33:30 +0100

    Added comments

--- src/main/java/assignment/Question1.java
+++ src/main/java/assignment/Question1.java
@@ -11,17 +11,22 @@ import java.util.List;
 
 public class Question1 {
     public static void main(String[] args) throws InterruptedException {
+        // web driver setup
         WebDriverManager.chromedriver().setup();
+
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://accounts.google.com/signup/v2/webcreateaccount?hl=en&flowName=GlifWebSignIn&flowEntry=SignUp");
+
+        // search all elements of type input by xpath - relative xpath
         List<WebElement> fields =
-                driver.findElements(By.xpath("//input[@type='text' or @type='password']"));
+                driver.findElements(By.xpath("//input[@type='text']"));
 
         for(WebElement element : fields) {
             System.out.println("Element Name: "+element.getAttribute("name")+" and Type: "+element.getAttribute("type"));
         }
 
+        // wait for 5 seconds using Duration class
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.close();
     }
